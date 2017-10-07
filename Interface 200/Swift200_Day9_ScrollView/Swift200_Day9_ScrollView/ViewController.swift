0db24f04b368ef81b5add6b61a7b95ff9cb3ce42
Swift <swift@wozmac4.local>
2017-10-07 18:06:58 -0300

    ULTIMOSARQUIVOSDAY9

--- Interface 200/Swift200_Day9_ScrollView/Swift200_Day9_ScrollView/ViewController.swift
+++ Interface 200/Swift200_Day9_ScrollView/Swift200_Day9_ScrollView/ViewController.swift
@@ -15,28 +15,47 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        montaCenario(sender:UIButton())
+    }
+    
+    func montaCenario(sender:UIButton) {
+    
         guard let cenario = uiCenario else {
             return
         }
+        
+        if !cenario.subviews.isEmpty {
+            cenario.subviews.forEach { imagem in
+                imagem.removeFromSuperview()
+            }
+        }
+        sender.removeFromSuperview()
+        
         let linhas = 10
         let colunas = 10
         
         for l in 0...linhas {
-           for c in 0...colunas {
-              let foto = UIImageView()
-              if l == 0 || c == 0 || c == 9 || l == 10 {
-                foto.frame  = CGRect(x:CGFloat(c*128),y:CGFloat(l*128),width:128,height:128)
-                foto.image  = UIImage(named:"tile\(arc4random_uniform(5)+15)")
-              }else {
-                foto.frame = CGRect(x:CGFloat(c*128),y:CGFloat(l*128),width:128, height:128)
-                foto.image = UIImage(named:"tile\(arc4random_uniform(20))")
-              }
-              cenario.addSubview(foto)
-           }
+            for c in 0...colunas {
+                let foto = UIImageView()
+                if l == 0 || c == 0 || c == 9 || l == 10 {
+                    foto.frame  = CGRect(x:CGFloat(c*128),y:CGFloat(l*128),width:128,height:128)
+                    foto.image  = UIImage(named:"tile\(arc4random_uniform(5)+15)")
+                }else {
+                    foto.frame = CGRect(x:CGFloat(c*128),y:CGFloat(l*128),width:128, height:128)
+                    foto.image = UIImage(named:"tile\(arc4random_uniform(20))")
+                }
+                cenario.addSubview(foto)
+            }
         }
         
+        let localImagem = cenario.subviews[Int(arc4random_uniform(100))].center
+        let gato = UIButton()
+        gato.frame = CGRect(origin: CGPoint(x:localImagem.x+68.0,y:localImagem.y+68.0), size: CGSize(width: 128, height: 128))
+        gato.setImage(UIImage(named:"mike"), for: .normal)
+        gato.addTarget(self, action: #selector(montaCenario(sender:)), for: .touchUpInside)
+        
+        print("\(localImagem)")
+        cenario.addSubview(gato)
     }
-
 }
 
