e49836f09fd279ebbe3617a23ca8954ac7534e11
System Administrator <root@iMac-Quaddro-A.local>
2018-01-27 17:05:43 -0200

    QUADDRONETWORK400SQLITEINITIAL

--- Network 400/WebService/SQLite/SQLite/ViewController.swift
+++ Network 400/WebService/SQLite/SQLite/ViewController.swift
@@ -10,16 +10,83 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nomeTextField: UITextField?
+    @IBOutlet weak var idadeTextField: UITextField?
+    @IBOutlet weak var logTextView: UITextView?
+    
+    // Será utilizado OpaquePointer pois como a library SQLite é escrita em C não possuimos uma representação em Swift
+    // Variável que representará a base de dados
+    var banco: OpaquePointer? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.logTextView?.text = nil
+        
+        // Resgatar o caminho da pasta de documents
+        let documentosURL = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        
+        // Criar o caminho do banco de dados
+        guard let caminhoDB = documentosURL?.appendingPathComponent("db.sqlite") else {
+            return
+        }
+        print(caminhoDB)
+        
+        // Funcao que cria ou abre um banco de dados SQLite
+        if sqlite3_open(caminhoDB.absoluteString, &self.banco) == SQLITE_OK {
+            self.exibir(log: "Abriu o banco")
+            
+            // Montar o comando de criação da tabela
+            let query = "CREATE TABLE IF NOT EXISTS PESSOAS (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nome TEXT, idade INTEGER)"
+            
+            // Executar o comando no DB
+            if sqlite3_exec(self.banco, query, nil, nil, nil) == SQLITE_OK {
+               self.exibir(log: "Tabela Criada")
+            } else {
+                self.exibir(log: "Falha na criação da tabela: \(String(cString: sqlite3_errmsg(self.banco!))) ")
+            }
+            
+        } else {
+            self.exibir(log: "Falha ao Criar/Abrir o banco: \(String(cString: sqlite3_errmsg(self.banco!)))")
+        }
     }
 
+    @IBAction func tapSalvar(_ sender: UIButton) {
+        // Garantir que possuímos nome e idade
+        guard let nome  = self.nomeTextField?.text,
+              let idade = self.idadeTextField?.text else {
+            return
+        }
+        // Converter idade de String para Int
+        let idadeInt = Int(idade)
+    
+        // Comando de inserção na tabela
+        let query = "INSERT INTO PESSOAS values (NULL,'\(nome)',\(idadeInt!))"
+        
+        if sqlite3_exec(self.banco!, query, nil, nil, nil) == SQLITE_OK {
+           self.exibir(log: "Registro adicionado")
+           self.nomeTextField?.text  = nil
+           self.idadeTextField?.text = nil
+        } else {
+           self.exibir(log: "Falha ao adicionar o registro \(String(cString: sqlite3_errmsg(self.banco!)))")
+        }
+    }
+    
+    @IBAction func tapResgatar(_ sender: UIButton) {
+        
+        // Comando para resgatar dos dados da tabela
+        let query = "SELECT * FROM PESSOAS"
+        
+        // Variável que armazenará o valor gerado pelo select
+        var statement:OpaquePointer?
+        
+    }
+    
+    func exibir(log:String) {
+        self.logTextView?.text.append("\n\(log)")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
